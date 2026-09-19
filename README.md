4dd4185c291074c3ef747853844bd096a7e2323d
felixvanmill <felix@van-mill.nl>
2026-09-19 11:20:36 +0200

    chore: configure environment variables and test database

---
+++ README.md
@@ -0,0 +1,10 @@
+Local setup
+
+1. Create a PostgreSQL database named:
+   afsprakenmanagement
+
+2. Configure the following environment variables:
+   DB_USERNAME=postgres
+   DB_PASSWORD=<your local PostgreSQL password>
+
+3. Run AfsprakenApplication.
---
+++ src/test/java/nl/outokumpu/afspraken/AfsprakenApplicationTests.java
@@ -0,0 +1,14 @@
+package nl.outokumpu.afspraken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AfsprakenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
